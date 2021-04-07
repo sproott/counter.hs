373e16a622bd5a3b7b06980bd6704a5ecc4d0270
sproott <david.hrabe12@seznam.cz>
2021-04-07 14:28:49 +0200

    🎨 Format State.hs

--- src/State.hs
+++ src/State.hs
@@ -5,11 +5,11 @@ module State where
 import           Lens.Micro.TH
 
 data State = State
-    { _isIncrement :: Bool,
-        _count     :: Int
+    { _isIncrement :: Bool
+    , _count       :: Int
     }
 
 makeLenses ''State
 
 getInitialState :: State
-getInitialState = State {_isIncrement = True, _count = 0}
+getInitialState = State { _isIncrement = True, _count = 0 }
